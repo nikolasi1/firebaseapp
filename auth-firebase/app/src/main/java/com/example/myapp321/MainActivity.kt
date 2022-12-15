081184cd20006a31b24474e04a95f4a86d1d8d84
nikolasi1 <116078116+nikolasi1@users.noreply.github.com>
2022-12-15 11:28:02 +0400

    Update MainActivity.kt

--- auth-firebase/app/src/main/java/com/example/myapp321/MainActivity.kt
+++ auth-firebase/app/src/main/java/com/example/myapp321/MainActivity.kt
@@ -1,46 +1,43 @@
-package com.example.myapp321
+package com.example.kacitamapatie
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
-import com.example.myapp321.R
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var email: EditText
+    private lateinit var password: EditText
     private lateinit var auth: FirebaseAuth
     private lateinit var firebaseAuth: FirebaseAuth
-    private lateinit var email: EditText
-    private lateinit var password : EditText
-    override fun onCreate(savedInstanceState: Bundle?) {
 
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        auth = Firebase.auth
-        email= findViewById(R.id.editTextEmail)
+        email = findViewById(R.id.editTextEmail)
         password= findViewById(R.id.editTextPassword)
-        firebaseAuth = FirebaseAuth.getInstance()
+        auth = Firebase.auth
+        firebaseAuth= FirebaseAuth.getInstance()
     }
-    public override fun onStart() {
-        super.onStart()
-        val currentUser = auth.currentUser
-        if(currentUser != null){
-        }
-    }
-    fun authFun(view:View){
-        if (password.text.toString().isNotEmpty() && email.text.toString().isNotEmpty()  && email.text.toString().contains("@") && email.text.toString().substring(email.text.toString().indexOf("@"), email.text.toString().length).contains(".")){ firebaseAuth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString()).addOnCompleteListener{
+    fun inputFunction(view:View){
+        val inputEmail = email.text.toString().isNotEmpty() && email.text.toString().contains("@")
+                && email.text.toString().substring(email.text.toString().indexOf("@"), email.text.toString().length).contains(".")
+        val inputPass = password.text.toString().isNotEmpty()
+        if (inputEmail && inputPass){
+            firebaseAuth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString()).addOnCompleteListener{
                 if(it.isSuccessful){
-                    Toast.makeText(this, "Successful", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "წარმატებით შეიქმნა", Toast.LENGTH_SHORT).show()
+
                 }else{
-                    Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "ახლიდან", Toast.LENGTH_SHORT).show()
                 }
             }
-
         }
     }
-
-
 }
